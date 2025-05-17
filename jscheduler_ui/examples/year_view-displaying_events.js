973d7d84ac100281403e4546de8907385b68bf5e
mormat <morelmathieuj@gmail.com>
2025-05-17 18:24:50 +0200

    jscheduler_ui 0.9.5

---
+++ jscheduler_ui/examples/year_view-displaying_events.js
@@ -0,0 +1,1 @@
+var element=document.getElementById("scheduler");jscheduler_ui.render(element,{viewMode:"year",events:[{label:"task 1",start:"2025-01-05 08:00",end:"2025-01-07 19:00",short_label:"t1"},{label:"task 2",start:"2025-03-10 08:00",end:"2025-03-10 19:00",short_label:"t2",bgColor:"coral"},null,{},{start:"invalid date"}],currentDate:"2025-10-10"});
---
+++ jscheduler_ui/examples/year_view-i18n.js
@@ -0,0 +1,1 @@
+var element=document.getElementById("scheduler");jscheduler_ui.render(element,{viewMode:"year",dateLocale:"fr"});
